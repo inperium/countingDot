36495e89f1b9721ba00e7bc46b2d00041b9bb08b
inperium <ibow9416@740s-pl-111.local>
2017-02-07 12:52:22 -0700

    Working on math

--- src/countingDot/controller/CountController.java
+++ src/countingDot/controller/CountController.java
@@ -1,18 +1,62 @@
 package countingDot.controller;
 
 import countingDot.view.CountFrame;
+import javax.swing.JOptionPane;
+import countingDot.model.*;
 
 public class CountController {
 
+	//Declaration
+	private RecursionTool recursionTool;
 	private CountFrame countFrame;
-
-	public void start() {
-	}
 	
+	//Constructor
 	public CountController() {
+		this.recursionTool = new RecursionTool();
 		countFrame = new CountFrame(this);
 	}
+	
+	//Start Method
+	public void start() {
+		
+	}
 
+	//Methods
+	public String transferFactorial(String input)
+	{
+		String factorialInfo = "The factorial " + input + " is ";
+		
+		if(isValid(input))
+		{
+			factorialInfo += recursionTool.calculateFactorial(Integer.parseInt(input));
+		}
+		
+		return factorialInfo;
+	}
+	
+	private boolean isValid(String testValue)
+	{
+		boolean asdf = false;
+		try
+		{
+			int test = Integer.parseInt(testValue);
+			if(test >= 0){
+				asdf = true;
+			}
+			else
+			{
+				asdf = false;
+			}
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(countFrame, "Type in a valid number greater than or equal to 0");
+			asdf = false;
+		}
+		return asdf;
+	}
+	
+	//Getters and Setters
 	public CountFrame getCountFrame() {
 		return countFrame;
 	}
--- src/countingDot/view/CountPanel.java
+++ src/countingDot/view/CountPanel.java
@@ -1,5 +1,7 @@
 package countingDot.view;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -24,7 +26,7 @@ public class CountPanel extends JPanel {
 		this.textArea = new JTextArea();
 		this.textField = new JTextField();
 		this.factButton = new JButton("Fact Button");
-		this.fibButton = new JButton("Fact Button");
+		this.fibButton = new JButton("Fib Button");
 
 		setupPanel();
 		setupLayout();
@@ -40,11 +42,25 @@ public class CountPanel extends JPanel {
 	}
 
 	private void setupListners() {
-
+		factButton.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					textArea.setText(baseController.transferFactorial(textField.getText()));
+				}
+			}
+		);
 	}
 
 	private void setupLayout() {
-
+		baseLayout.putConstraint(SpringLayout.NORTH, textField, 190, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, textField, 105, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, textField, -89, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, textField, -19, SpringLayout.NORTH, factButton);
+		baseLayout.putConstraint(SpringLayout.SOUTH, factButton, -25, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, factButton, -70, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, fibButton, 0, SpringLayout.NORTH, factButton);
+		baseLayout.putConstraint(SpringLayout.EAST, fibButton, -66, SpringLayout.WEST, factButton);
 	}
 
 }
